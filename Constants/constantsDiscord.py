b8da0d82b02dadc38636b1f969899253659aa4be
Michael Ge <56697279+Spitfier720@users.noreply.github.com>
2025-07-09 02:10:38 -0400

    Quick Filepath Fix

--- Constants/constantsDiscord.py
+++ Constants/constantsDiscord.py
@@ -1,7 +1,7 @@
 itemsWorthNotifying = {
     "Screenshots/Gear Items/GearTanning.png": "Tanning Mirror",
     "Screenshots/Gear Items/GearMaster.png": "Master Sprinkler",
-    "Screenshots/Seeds.SeedEmber.png": "Ember Lily",
+    "Screenshots/Seeds/SeedEmber.png": "Ember Lily",
     "Screenshots/Seeds/SeedSugar.png": "Sugar Apple",
     "Screenshots/Seeds/SeedBurning.png": "Burning Bud"
 }
